73d2a54560f361bffb3812024ea76dd1e1476e64
Manuel Sanchez <manuelsfdelavega@gmail.com>
2025-02-23 17:15:36 -0300

    refactor: use relative routes instead of absolutes in init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,3 @@
-from app.models.base import BaseModel as BaseModel
-from app.models.expense import Expense as Expense
-from app.models.user import User as User
+from .base import BaseModel as BaseModel
+from .expense import Expense as Expense
+from .user import User as User
