f3b5a73423e3901599ccac58fa73f8c26739e076
Manuel Sanchez <manuelsfdelavega@gmail.com>
2025-02-23 21:49:07 -0300

    fix: add exclude rule to ruff for models init.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,4 @@
-from .api_key import ApiKey as ApiKey
 from .base import BaseModel as BaseModel
 from .expense import Expense as Expense
 from .user import User as User
+from .api_key import ApiKey as ApiKey
