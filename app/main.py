9fe6f4d0d398c0a1f41dacad37bae57cbc1c995f
Manuel Sanchez <manuelsfdelavega@gmail.com>
2025-02-23 21:40:54 -0300

    refactor: rename db.py to database.py

--- app/main.py
+++ app/main.py
@@ -15,23 +15,21 @@ from app.schemas import UserMessage
 
 load_dotenv()
 
-json_parser = ExpenseJsonOutputParser()
-model = ChatAI21(model="jamba-instruct")
-parser = ExpenseParser(model, json_parser)
-
-
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 POSTGRES_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}"
 
+json_parser = ExpenseJsonOutputParser()
+model = ChatAI21(model="jamba-instruct")
+parser = ExpenseParser(model, json_parser)
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     db.initialize(POSTGRES_URL)
     yield
-    db.close()
 
 
 app = FastAPI(lifespan=lifespan)
